bfeb90cc87a8a1fd3065050b80f6d122561752be
ivankp <ivan.pogrebnyak@gmail.com>
2017-05-21 15:50:43 -0400

    seems to work

--- src/rivet_ntuples.cc
+++ src/rivet_ntuples.cc
@@ -17,35 +17,21 @@
 #include "timed_counter.hh"
 #include "float_or_double_reader.hh"
 
-#define test(var) \
+#define TEST(var) \
   std::cout <<"\033[36m"<< #var <<"\033[0m"<< " = " << var << std::endl;
 
 using std::cout;
 using std::cerr;
 using std::endl;
 
-template <typename T, typename F>
-auto operator%(const std::vector<T>& v, F&& f) {
-  std::vector<decltype(f(std::declval<const T&>()))> out;
-  out.reserve(v.size());
-  for (const auto& x : v) out.emplace_back(f(x));
-  return std::move(out);
-}
-template <typename T, typename F>
-auto operator%(std::vector<T>& v, F&& f) {
-  std::vector<decltype(f(std::declval<T&>()))> out;
-  out.reserve(v.size());
-  for (auto& x : v) out.emplace_back(f(x));
-  return std::move(out);
-}
-
 // https://github.com/danielmaitre/nTuple2Rivet/blob/master/nTuple2Rivet.cpp
 // https://github.com/danielmaitre/nTupleReader/blob/master/nTupleReader_impl.cpp#L214
 
 int main(int argc, char* argv[]) {
   if (argc<4) {
-    cout << "usage: " << argv[0] << " rivet_analysis rivet_output ntuple.root"
-     " [(ntuple|weights).root]" << endl;
+    cout << "usage: " << argv[0]
+         << " rivet_analysis_name rivet_output.yoda ntuple.root"
+            " [(ntuple|weights).root]" << endl;
     return 1;
   }
 
@@ -99,11 +85,13 @@ int main(int argc, char* argv[]) {
   else {
     const TObjArray *bb = weights_chain->GetListOfBranches();
     _weights.reserve(bb->GetEntriesFast());
-    for (const auto* b : *bb) {
+    for (const auto* b : *bb)
       _weights.emplace_back(reader,static_cast<const TBranch*>(b)->GetName());
-    }
   }
-  // std::
+  cout << "\033[36mWeights\033[0m:" << endl;
+  for (const auto& w : _weights)
+    cout << "  " << w.GetBranchName() << endl;
+  cout << endl;
 
   Rivet::AnalysisHandler rivet;
   rivet.addAnalysis(argv[1]);
@@ -117,35 +105,34 @@ int main(int argc, char* argv[]) {
     cout << "  " << name << endl;
   cout << endl;
 
-  
+  HepMC::GenEvent event;
+  event.use_units(HepMC::Units::GEV, HepMC::Units::MM);
+
   // LOOP ===========================================================
   using tc = ivanp::timed_counter<Long64_t>;
   for (tc ent(reader.GetEntries(true)); reader.Next(); ++ent) {
     using namespace HepMC;
 
-    GenVertex* v = new GenVertex();
-    GenParticle
-      *gp1 = new GenParticle(6500, 2212, 4),
-      *gp2 = new GenParticle(6500, 2212, 4);
-    v->add_particle_in(gp1);
-    v->add_particle_in(gp2);
+    // set weights
+    event.weights().clear();
+    for (auto& w : _weights) event.weights().push_back(*w);
 
-    GenEvent event(
-      Units::GEV, Units::MM,
-      0, /*signal_process_id*/
-      *_id, /*event_number*/
-      v, /*signal_vertex*/
-      _weights % [](auto& w){ return *w; } /*weights*/
-    );
-    event.set_beam_particles(gp1,gp2);
+    GenVertex* v = new GenVertex;
+    event.add_vertex(v);
+    GenParticle *p1 = new GenParticle(6500, 2212, 4);
+    GenParticle *p2 = new GenParticle(6500, 2212, 4);
+    v->add_particle_in(p1);
+    v->add_particle_in(p2);
+    event.set_beam_particles(p1,p2);
 
     for (int i=0, n=*_nparticle; i<n; ++i) {
-      v->add_particle_out(
-        new GenParticle({_px[i],_py[i],_pz[i],_E[i]}, _kf[i], 1));
-      // p->set_generated_mass(0);
+      GenParticle *p = new GenParticle({_px[i],_py[i],_pz[i],_E[i]},_kf[i],1);
+      p->set_generated_mass(0);
+      v->add_particle_out(p);
     }
 
     rivet.analyze(event);
+    event.clear();
   } // END LOOP
 
   rivet.finalize();
